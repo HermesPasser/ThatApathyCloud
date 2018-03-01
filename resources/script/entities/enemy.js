b731403a48e286dc793de621f13c1c43ee395630
Hermes Passer <dsldouglas@hotmail.com>
2018-02-28 23:25:59 -0300

    Create enemy turret.

--- resources/script/entities/enemy.js
+++ resources/script/entities/enemy.js
@@ -119,14 +119,8 @@ class Enemy extends CharacterBase{
 			return;
 		
 		let distance = RamuMath.distance(GameScreen.player, this);
-		
-		// console.log("antes " + this.canPass)
-		
-		this.isPassable();
-		
-		// console.log("depois " + this.canPass)
-		// console.log("------------")
-		
+			
+		this.isPassable();	
 		this.machineState(true);
 		
 		// Move
@@ -182,5 +176,69 @@ class Enemy extends CharacterBase{
 		this.mainCol.destroy()
 		this.destroy();
 		delete this.mainCol;
+	}
+}
+
+class LeftTurret extends Enemy{
+	static create(TiledXMLObject){
+		let enemy = new LeftTurret(TiledXMLObject.x, TiledXMLObject.y);
+		enemy.animDrawPriority = GameScreen.player.animDrawPriority;
+		
+		return enemy;
+	}
+	
+	start(){
+		super.start();
+		this.shootParticle = new SimpleParticle(PARTICLE_IMAGE.YELLOW, new Rect(1, 1, 1, 1), 3, shotParticleNumber);
+		
+		this.width = 50;
+		this.height = 50;
+		this.mainCol.width = 50;
+		this.mainCol.x += 34;
+		this.isTurret = true;
+	}
+	
+	applyDamage(damage){ }
+	
+	setupAnimation(){
+		let normal = new SpritesheetAnimation(BASIS.IMAGE, 0, 200, 84, 50);
+		normal.addFrame(new Rect(0, 200, 84, 50));
+		
+		let broken = new SpritesheetAnimation(BASIS.IMAGE, 0, 250, 84, 50);
+		broken.addFrame(new Rect(0, 250, 84, 50));
+
+		this.addAnimation("normal", normal);
+		this.addAnimation("broken", broken);
+		
+		this.setCurrentAnimation("normal");
+	}
+	
+	shot(){
+		if (this.currentTimeToShot < this.timeToShot)
+			return;
+				
+		let x = this.x, y = this.y + 22;
+	
+		new Bullet(x, y, DIRECTION.LEFT, this.getDamage(), this.getEnemyTag());
+		this.currentTimeToShot = 0;	
+		
+		this.shootParticle.setPosition(this.x, this.y + this.height/2);
+		this.shootParticle.init();
+	}
+	
+	ia(){
+		if (!GameScreen.player)
+			return;
+		
+		let distance = RamuMath.distance(GameScreen.player, this);
+				
+		// Shot if the player is in a range of 4 floor tiles
+		if (!(distance > 200 && distance < 800) || GameScreen.player.isInvisible)
+			this.shot();
+	}
+	
+	update(){
+		this.ia();
+		this.currentTimeToShot += Ramu.time.delta;
 	}
 }
