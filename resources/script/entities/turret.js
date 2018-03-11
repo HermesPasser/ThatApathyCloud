fb3184049f54fd3f78dd111b75d435d33bba9464
Hermes Passer <dsldouglas@hotmail.com>
2018-03-11 20:19:12 -0300

    changed: bullet can be recycled; turrets uses the same bullets instead of instance one each time when shoot.

--- resources/script/entities/turret.js
+++ resources/script/entities/turret.js
@@ -10,10 +10,16 @@ class LeftTurret extends Enemy{
 		super(x,y);
 		this.turretID = parseInt(id);
 	}
-	
+
 	start(){
 		super.start();
 		this.shootParticle = new SimpleParticle(PARTICLE_IMAGE.YELLOW, new Rect(1, 1, 1, 1), 3, shotParticleNumber);
+		
+		// Bullets
+		this.currentBullets = 0;
+		this.bullets = [];
+		for (var i = 0; i < 5; i++)
+			this.bullets.push(new Bullet(0, 0, false));
 		
 		// To set the body of turret in the position instead of barrel
 		this.setX(this.x - 33);
@@ -30,6 +36,17 @@ class LeftTurret extends Enemy{
 	die() {
 		this.setCurrentAnimation("broken");
 		this.broken = true;
+	}
+	
+	destroy(){
+		this.shootParticle.destroy();
+		delete this.shootParticle;
+		
+		for (var i = 0; i < this.bullets.length; i++)
+			this.bullets[i].destroy();
+		delete this.bullets;
+		
+		super.destroy();
 	}
 	
 	setupAnimation(){
@@ -51,7 +68,8 @@ class LeftTurret extends Enemy{
 				
 		let x = this.x, y = this.y + 22;
 	
-		new Bullet(x, y, DIRECTION.LEFT, this.getDamage(), this.getEnemyTag());
+		this.bullets[this.currentBullets].init(x, y, DIRECTION.LEFT, this.getDamage(), this.getEnemyTag());
+		this.currentBullets = (this.currentBullets + 1) % this.bullets.length;
 		this.currentTimeToShot = 0;	
 		
 		this.shootParticle.setPosition(this.x, this.y + this.height/2);
@@ -75,5 +93,57 @@ class LeftTurret extends Enemy{
 		
 		this.ia();
 		this.currentTimeToShot += Ramu.time.delta;
+	}
+}
+
+// broken.flipHorizontally = true;
+class UpTurret extends LeftTurret{
+	static create(TiledXMLObject){
+		let turret = new UpTurret(TiledXMLObject.x, TiledXMLObject.y, TiledXMLObject.properties["turretID"]);
+		turret.animDrawPriority = GameScreen.player.animDrawPriority;
+		return turret;
+	}
+	
+	start(){
+		super.start();
+		this.shootParticle = new SimpleParticle(PARTICLE_IMAGE.YELLOW, new Rect(1, 1, 1, 1), 3, shotParticleNumber);
+		
+		// To set the body of turret in the position instead of barrel
+		this.setX(this.x + 33);
+		this.setY(this.y - 34);
+		
+		this.width = 50;
+		this.height = 50;
+		this.mainCol.width = 50;
+		this.mainCol.y += 34;
+		this.isTurret = true;
+		this.broken = false;
+	}
+
+	setupAnimation(){
+		let normal = new SpritesheetAnimation(BASIS.IMAGE, 0, 200, 50, 84);
+		normal.addFrame(new Rect(0, 100, 50, 84));
+		
+		let broken = new SpritesheetAnimation(BASIS.IMAGE, 0, 250, 50, 84);
+		broken.addFrame(new Rect(50, 100, 50, 84));
+
+		this.addAnimation("normal", normal);
+		this.addAnimation("broken", broken);
+		
+		this.setCurrentAnimation("normal");
+	}
+	
+	shot(){
+		if (this.currentTimeToShot < this.timeToShot)
+			return;
+				
+		let x = this.x + 22, y = this.y;
+	
+		this.bullets[this.currentBullets].init(x, y, DIRECTION.TOP, this.getDamage(), this.getEnemyTag());
+		this.currentBullets = (this.currentBullets + 1) % this.bullets.length;
+		this.currentTimeToShot = 0;	
+		
+		this.shootParticle.setPosition(x + 2, y);
+		this.shootParticle.init();
 	}
 }
